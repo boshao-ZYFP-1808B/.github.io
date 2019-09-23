6a248acfd239937ac3218600d80fd5be171d752e
zhaojiaershao <zhaoruibo123>
2019-09-23 16:51:21 +0800

    ojbk

---
+++ build/precache-manifest.fbc2b2e000847d920adef51847c55bcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2e29d51a8451c5ed6cb3524eaacd41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c09585d479b17692371",
+    "url": "./static/css/2.96121624.chunk.css"
+  },
+  {
+    "revision": "07400ec215e881f42f12",
+    "url": "./static/css/main.dc5c3e06.chunk.css"
+  },
+  {
+    "revision": "5c09585d479b17692371",
+    "url": "./static/js/2.a9b32bc6.chunk.js"
+  },
+  {
+    "revision": "07400ec215e881f42f12",
+    "url": "./static/js/main.67f5c13e.chunk.js"
+  },
+  {
+    "revision": "8525b147b5989b8b3880",
+    "url": "./static/js/runtime-main.194240a4.js"
+  },
+  {
+    "revision": "b8580516b3c6c861cbf3d7bc1d064cb7",
+    "url": "./static/media/login.b8580516.jpg"
+  },
+  {
+    "revision": "e5fdc4d9d60a6d7da63d35d1863347c6",
+    "url": "./static/media/图片.e5fdc4d9.jpg"
+  }
+]);
